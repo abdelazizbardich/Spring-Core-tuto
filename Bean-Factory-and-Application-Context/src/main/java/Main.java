166941239a48b95425d207ed99e4041a1d9f5203
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-02-08 12:01:36 +0100

    Bean Factory with spring.xml by FileSystemXmlApplicationContext

--- Bean-Factory-and-Application-Context/src/main/java/Main.java
+++ Bean-Factory-and-Application-Context/src/main/java/Main.java
@@ -2,12 +2,14 @@ import ma.youcode.beans.User;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class Main {
     public static void main(String[] args) {
 //        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring.xml")); // deprecated
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring.xml"); // recomended
+//        BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring.xml"); // recommended
+        BeanFactory beanFactory = new FileSystemXmlApplicationContext("src/main/resources/spring.xml"); // to give a custom xml file location
         User user = beanFactory.getBean("user",User.class);
         user.SayHello("john");
     }
